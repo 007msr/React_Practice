2326324b5f61e7317511c70c966d41d74195f3ac
007msr <80089662+007msr@users.noreply.github.com>
2021-12-08 23:50:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import react, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("Mukesh");
+  const changeName = () => {
+    if (name === "Mukesh") setName("Jugnu");
+    else setName("Mukesh");
+  };
+
+  return (
+    <div>
+      <h1> {name}</h1>
+      <button onClick={changeName}> Click Here To Change Name</button>
+    </div>
+  );
+}
+
+export default App;
